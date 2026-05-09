ed93c868fdc185868b21fb5b531287694582a951
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-09 11:07:41 +0800

    fix: add database url check

--- apps/api/src/auth/auth.ts
+++ apps/api/src/auth/auth.ts
@@ -3,8 +3,14 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is not set');
+}
+
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL as string,
+  connectionString: databaseUrl as string,
 });
 
 const prisma = new PrismaClient({ adapter });
