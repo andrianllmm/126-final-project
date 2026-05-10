9dae2b3e4cf9ad5480cd5d89c8a57f19b6844e51
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-10 22:53:15 +0800

    refactor: move content feature to features

--- apps/web/app/(public)/about/page.tsx
+++ apps/web/app/(public)/about/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import { AboutPage } from '@/content/about/about-page';
+import { AboutPage } from '@/features/content/about/about-page';
 
 export const metadata: Metadata = {
   title: 'About Iskommerce',
