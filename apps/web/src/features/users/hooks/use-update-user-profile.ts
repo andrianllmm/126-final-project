2a68f303599961203c7fda8b85ea31ef7ce5a825
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-15 11:46:46 +0800

    fix(users): remove auth from useUpdateUserProfile hook

--- apps/web/src/features/users/hooks/use-update-user-profile.ts
+++ apps/web/src/features/users/hooks/use-update-user-profile.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateUserProfile } from '../api/users-api';
-import { authClient } from '@/shared/lib/auth-client';
 import type { UserProfile, UserProfileUpdateInput } from '@repo/api';
 
 type UseUpdateUserProfileParams = {
@@ -9,15 +8,12 @@ type UseUpdateUserProfileParams = {
 
 export function useUpdateUserProfile({ userId }: UseUpdateUserProfileParams) {
   const queryClient = useQueryClient();
-  const session = authClient.useSession();
 
   return useMutation({
     mutationFn: (values: UserProfileUpdateInput) => updateUserProfile(values),
 
     onSuccess: async (updatedProfile: UserProfile) => {
       queryClient.setQueryData(['users', 'profile', userId], updatedProfile);
-
-      session.refetch();
     },
   });
 }
