265a9903263c4543eac81bd74a19402d63ca92b1
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-16 23:41:44 +0800

    refactor(auth): move change password schema in shared package

--- packages/api/src/entry.ts
+++ packages/api/src/entry.ts
@@ -4,3 +4,4 @@ export * from './users/profile.schema.js';
 export * from './listings/listings.schema.js';
 export * from './auth/sign-in.schema.js';
 export * from './auth/sign-up.schema.js';
+export * from './auth/change-password.schema.js';
