65baf55c8a30f956cc60ee24a70337acf10f08ea
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-17 12:21:35 +0800

    feat(backend/messaging): update messaging module

--- apps/api/src/modules/messaging/messaging.module.ts
+++ apps/api/src/modules/messaging/messaging.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MessagingController } from './messaging.controller.js';
+import { MessagingGateway } from './messaging.gateway.js';
 import { MessagingService } from './messaging.service.js';
+import { MessagingController } from './messaging.controller.js';
+import { DatabaseModule } from '../../database/database.module.js';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [MessagingController],
-  providers: [MessagingService],
+  providers: [MessagingGateway, MessagingService],
+  exports: [MessagingService],
 })
 export class MessagingModule {}
