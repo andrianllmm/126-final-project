cf84e7c1d3918955c367179daec368f9a0613e03
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-20 22:14:17 +0800

    fix(frontend): next image config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,24 +1,36 @@
 /** @type {import('next').NextConfig} */
 import createMDX from '@next/mdx';
 
+const isDev = process.env.NODE_ENV === 'development';
+
+const apiUrl = new URL(process.env.NEXT_PUBLIC_API_URL);
+
 const nextConfig = {
   allowedDevOrigins: ['http://localhost:3000'],
   output: 'standalone',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+
   images: {
     remotePatterns: [
+      {
+        protocol: apiUrl.protocol.replace(':', ''),
+        hostname: apiUrl.hostname,
+        port: apiUrl.port || '',
+        pathname: '/uploads/**',
+      },
       {
         protocol: 'https',
         hostname: '**',
       },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
     ],
+
+    ...(isDev
+      ? {
+          dangerouslyAllowLocalIP: true,
+        }
+      : {}),
   },
 };
 
 const withMDX = createMDX({});
-
 export default withMDX(nextConfig);
