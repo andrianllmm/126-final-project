ee5316a2919ac0b6045ba95aecd878476c0e5c96
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-26 19:00:44 +0800

    feat(frontend/listings): update listing card UI

--- apps/web/src/features/listings/components/listing-card-skeleton.tsx
+++ apps/web/src/features/listings/components/listing-card-skeleton.tsx
@@ -17,48 +17,46 @@ export function ListingCardSkeleton({ className }: ListingCardSkeletonProps) {
   return (
     <Card
       className={cn(
-        'overflow-hidden border bg-background p-0 gap-1',
+        'cursor-pointer overflow-hidden border bg-background p-0 gap-1',
+        'transition-all duration-200',
         className,
       )}
     >
       {/* IMAGE */}
       <div className="relative aspect-4/3 overflow-hidden bg-muted">
         <Skeleton className="h-full w-full" />
-
-        {/* badges */}
-        <div className="absolute left-3 top-3 flex gap-2">
-          <Skeleton className="h-5 w-16 rounded-full" />
-          <Skeleton className="h-5 w-16 rounded-full" />
-        </div>
       </div>
 
       {/* HEADER */}
       <CardHeader className="space-y-3 px-4 pt-4">
         <div className="flex items-start justify-between gap-3">
           <div className="flex-1 space-y-2">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-3/4" />
+            <Skeleton className="h-4 w-[80%]" />
           </div>
 
-          <Skeleton className="h-6 w-20 shrink-0" />
+          <Skeleton className="h-5 w-12 shrink-0" />
         </div>
       </CardHeader>
 
       {/* CONTENT */}
       <CardContent className="space-y-3 px-4">
         <div className="space-y-2">
-          <Skeleton className="h-3 w-full" />
-          <Skeleton className="h-3 w-5/6" />
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Skeleton className="h-6 w-6 rounded-full" />
-          <Skeleton className="h-4 w-28" />
+          <Skeleton className="h-3 w-[70%]" />
+          <Skeleton className="h-3 w-[40%]" />
         </div>
       </CardContent>
 
       {/* FOOTER */}
-      <CardFooter className="px-4 pb-4 pt-4">
+      <CardFooter className="flex flex-col gap-2 px-4 pb-4 mt-auto">
+        <div className="w-full flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Skeleton className="size-6 rounded-full" />
+            <Skeleton className="h-3 w-18" />
+          </div>
+
+          <div></div>
+        </div>
+
         <Skeleton className="h-9 w-full" />
       </CardFooter>
     </Card>
